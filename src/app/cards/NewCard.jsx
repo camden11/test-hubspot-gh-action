305bb23e1b9fa4fea9d9c0c58599ce2eec104ccf
Camden Phalen <cphalen@hubspot.com>
2025-08-19 10:58:43 -0400

    Add new card component

--- src/app/cards/NewCard.jsx
+++ src/app/cards/NewCard.jsx
@@ -8,7 +8,7 @@ const Extension = () => {
   return (
     <>
       <Text>
-        Congrats! Here is your new card! Test
+        Congrats! Here is your new card! Test hello
       </Text>
     </>
   );
